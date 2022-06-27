b9f24b8af335a747401ee06a1551bea5d4179fdd
Diosel Torrefranca <dioseltorre@gmail.com>
2022-06-27 10:08:14 +0900

    updates

--- pocket-change/safe-area/flutter_service_worker.js
+++ pocket-change/safe-area/flutter_service_worker.js
@@ -355,13 +355,13 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b1f551467b53b59fb06609bed4bdbd00",
-"/": "b1f551467b53b59fb06609bed4bdbd00",
+"index.html": "7b42d5def0c4dd70c5bfc5b41e791bd8",
+"/": "7b42d5def0c4dd70c5bfc5b41e791bd8",
 "loading/loader.css": "26dcfc9e1794554e3552c772bd4c2b03",
 "loading/loader.js": "d73f0c045b1a2397ea931859e5a8da12",
 "loading/pokepay-logo.svg": "56d8221b9ec4e17570bd0144bf898b8e",
 "loading/pokepay.svg": "b7b8538e259f3a4ee0b3a92a3ecbbdca",
-"main.dart.js": "479cd3269361342844d0e9e99ca1eb7f",
+"main.dart.js": "572c98b2a455f983b4d50179a22782de",
 "manifest.json": "dcd4a3f4165165a646a2e931c0e919f2",
 "version.json": "3c476c38e3bb9d9eff72274aa593a9ee"
 };
